7f18c9fb532ac5d8a50353970f3177c7ae3b1c31
Armando Lopez <rlopez@hightech-corp.com>
2020-10-01 14:55:08 -0600

    update prometrics.sh

--- bin/prometrics.sh
+++ bin/prometrics.sh
@@ -9,17 +9,18 @@ if [ -f $FILE ]; then
     cat $REPORT/result.json | \
     # Extract a nice json from last report
     #jq -r '.projectName as $project |.environment as $env | .specResults[] |"test_QA {env=\"\($env)\", spec=\"\(.specHeading|gsub("\"";"\\\""))\", result=\"\(.executionStatus)\"} \(.executionTime)"' | \
-    jq -r '.projectName as $project |.environment as $env | .specResults[] |.specHeading as $spec| .scenarios[] |.scenarioHeading as $scen|.items[]| select(.itemType=="step") |.stepText as $step|"test_qa{env=\"\($env)\", project=\"\($proejct|gsub("\"";"\\\""))\", operation=\"qa\", spec=\"\($spec|gsub("\"";"\\\""))\", scenario=\"\($scen|gsub("\"";"\\\""))\, step=\"\($step|gsub("\"";"\\\""))\", result=\"\(.result.status)\"} \(.result.executionTime)"' | \
-    #Send to the emtric server
-    curl -v --data-binar @- $PUSH_GATE/metrics/job/ngt
+    jq -r '.projectName as $project |.environment as $env | .specResults[] |.specHeading as $spec| .scenarios[] |.scenarioHeading as $scen|.items[]| select(.itemType=="step") |.stepText as $step|"test_QA{env=\"\($env)\", project=\"\($project|gsub("\"";"\\\""))\", operation=\"du\", spec=\"\($spec|gsub("\"";"\\\""))\", scenario=\"\($scen|gsub("\"";"\\\""))\", step=\"\($step|gsub("\"";"\\\""))\", result=\"\(.result.status)\"}  \(.result.executionTime)"' | \
+    # Send to the metric server
+    curl -v --data-binary @- $PUSH_GATE/metrics/job/ngt
     name=`uname -n`
     timestamp=`date +%s`
     result=`jq -r '.executionStatus' $REPORT/result.json`
-    echo "prometics_exec{name\"$name\", result=\"$result\"} $timestamp" | curl --data-binary @- $PUSH_GATE/metrics/job/ngt_prometrics
-    cp -r /test_QA/test_QA/reports/html-report/* /root/test_QA/HttpsShared/
-    cp -r /test_QA/test_QA/reports/html-report /root/test-QA/HttpsShared/html-report.$DATE
-    #usermod -u 101 /root/test_QA/HttpShard/
-    cd /root/test_QA/HttpShard/ & chown 101:101 -R *
+    echo "prometrics_exec{name=\"$name\", result=\"$result\"} $timestamp" | curl --data-binary @- $PUSH_GATE/metrics/job/ngt_prometrics
+    cp -r /test_QA/test_QA/reports/html-report/*  /root/test_QA/HttpShared/
+    cp -r /test_QA/test_QA/reports/html-report /root/test_QA/HttpShared/html-report.$DATE    
+    # usermod -u 101 /root/ngt-bcr-qa/HttpShared/
+    cd /root/test_QA/HttpShared/ & chown 101:101 -R *
+
 
 else
     echo "File $FILE does not exist."
