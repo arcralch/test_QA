deb5ec7f8e02eabb029e58d7da78d903ebad1cea
Armando Lopez <rlopez@hightech-corp.com>
2020-10-01 16:21:14 -0600

    update bin

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo $(date)
+echo $CASES
+echo $ENV
+echo $METRIC_COMPL
 cd /test_QA/test_QA && git pull
-cd /test_QA/test_QA && mvn test
+cd /ngt-codex-qa/ngt-codex-qa && mvn clean
+cd /ngt-codex-qa/ngt-codex-qa && mvn package -DskipTests 
+cd /ngt-codex-qa/ngt-codex-qa && mvn gauge:execute -DspecsDir=specs/$CASES -Denv=$ENV
 sh /test_QA/test_QA/bin/prometrics.sh
 sh /test_QA/test_QA/bin/checkiotenv.sh
